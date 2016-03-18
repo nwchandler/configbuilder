6a26ec03eb3048c83760bacbf358be1e02646540
Nick Chandler <nchandler@burwood.com>
2016-03-17 22:38:31 -0400

    Support xlyaml buildOutput unit test
    In order to make it easier to maintain code over time, we need to have
    unit testing support for methods and functions. This is a start.

---
+++ tests/test_xlyaml_collection_class.py
@@ -0,0 +1,18 @@
+import yaml
+import xlyaml
+
+def test_collection_class_buildOutput_builds_valid_yaml():
+	sheet_object = [ \
+		['key1', 'value1', None, None], \
+		['key2', 'value2', None, None], \
+		['list1', None,  None, None], \
+		[None, 'l1_item1', None, None], \
+		[None, 'l1_item2', None, None], \
+		[' dict1', 'd1_key1', 'd1_key2', 'd1_key3'], \
+		[None, 'd1_k1_v1', 'd1_k2_v1', 'd1_k3_v1'], \
+		[None, 'd1_k1_v2', 'd1_k2_v2', 'd1_k3_v2'] \
+		]
+	collection_object = xlyaml.Collection(sheet_object)
+
+	assert collection_object._this_collection == \
+		yaml.load(collection_object.buildOutput())
